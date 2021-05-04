5c7db3d13dea864be6088516c46c7aab4b4bb608
adriencances <adriencances@gmail.com>
2021-05-04 13:59:35 +0200

    Python file to generate positive pairs

--- link_tracks_with_GT.py
+++ link_tracks_with_GT.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 GT_dir = "/home/acances/Data/TVHID/tv_human_interactions_annotations"
 tracks_dir = "/home/acances/Data/TVHID/tracks"
-new_tracks_dir = "/home/acances/Data/TVHID/new_tracks"
+track_linking_dir = "/home/acances/Data/TVHID/GT_tracks_to_det_tracks"
 
 
 def get_GT_tracks(GT_file):
@@ -59,28 +59,72 @@ def mean_IoGT(det_track, GT_track):
     for i in range(len(det_track)):
         frame_id = int(det_track[i][0])
         det_box = det_track[i][1:5]
-        if frame_id not in GT_track.keys():
+        if frame_id not in GT_track:
             continue
         GT_box = GT_track[frame_id]
         IoGTs[i] = intersection_over_GT_area(det_box, GT_box)
     return sum(IoGTs) / len(IoGTs)
 
 
+def time_IoGT(det_track, GT_track):
+    det_b = det_track[0, 0]
+    det_e = det_track[-1, 0]
+    GT_b = min(GT_track.keys())
+    GT_e = max(GT_track.keys())
+
+    b = max(det_b, GT_b)
+    e = min(det_e, GT_e)
+    inter = max(0, e - b + 1)
+
+    return inter / len(GT_track)
+
+
 def link_tracks(video_id):
-    tracks_files = glob.glob("{}/{}/*".format(tracks_dir, video_id))
+    tracks_files = sorted(glob.glob("{}/{}/*".format(tracks_dir, video_id)))
     det_tracks = []
     for tracks_file in tracks_files:
         with open(tracks_file, "rb") as f:
             det_tracks += pickle.load(f)
+    det_scores = [e[1] for e in det_tracks]
     det_tracks = [e[0] for e in det_tracks]
 
     GT_file = "{}/{}.annotations".format(GT_dir, video_id)
     GT_tracks = get_GT_tracks(GT_file)
 
-    for det_track in det_tracks:
+    time_IoGTs = []
+    GT_to_det = {}
+    for det_track_id, det_track in enumerate(det_tracks):
+        if len(GT_to_det) == len(GT_tracks):
+            break
         mean_IoGTs = [mean_IoGT(det_track, GT_track) for GT_track in GT_tracks]
-        print(mean_IoGTs)
+        indices = list(reversed(np.argsort(mean_IoGTs)))
+        best_GT_track_id = indices[0]
+        if mean_IoGTs[best_GT_track_id] < 0.4:
+            continue
+        if best_GT_track_id not in GT_to_det:
+            GT_to_det[best_GT_track_id] = (det_track_id, mean_IoGTs[best_GT_track_id])
+            time_IoGTs.append(time_IoGT(det_track, GT_tracks[best_GT_track_id]))
+
+        # print(mean_IoGTs, det_scores[det_track_id])
+        # print(list(reversed(np.argsort(mean_IoGTs))))
+        # print(np.argmax(mean_IoGTs))
+    
+    output_file = "{}/{}_track_links.pkl".format(track_linking_dir, video_id)
+    with open(output_file, "wb") as f:
+        pickle.dump(GT_to_det, f)
+
+    # print(np.mean(time_IoGTs))
+    # # for key in GT_to_det:
+    # #     print(GT_to_det[key])
+    # if len(GT_to_det) != len(GT_tracks):
+    #     # print("INCOMPLETE")
+    #     print(video_id)
 
 if __name__ == "__main__":
-    video_id = sys.argv[1]
-    link_tracks(video_id)
+    if len(sys.argv) > 2:
+        video_id = sys.argv[1]
+        link_tracks(video_id)
+    else:
+        video_ids = [e.split("/")[-1] for e in sorted(glob.glob("{}/*".format(tracks_dir)))]
+        for video_id in video_ids:
+            link_tracks(video_id)
---
+++ pairs_generation/generate_positive_pairs.py
@@ -0,0 +1,122 @@
+import cv2
+import numpy as np
+import pickle
+import sys
+import glob
+from pathlib import Path
+
+
+#  PARAMETERS
+SEGMENT_LENGTH = 16
+ACCEPTABLE_DELAY = SEGMENT_LENGTH // 2
+
+
+GT_dir = "/home/acances/Data/TVHID/tv_human_interactions_annotations"
+tracks_dir = "/home/acances/Data/TVHID/tracks"
+track_linking_dir = "/home/acances/Data/TVHID/GT_tracks_to_det_tracks"
+pairs_dir = "/home/acances/Data/TVHID/pairs{}".format(SEGMENT_LENGTH)
+
+
+def get_interactions(video_id):
+    # Each element in the returned vector "interactions" si of the form
+    # [id_1, id_2, b, e]
+    # where id_1 and id_2 are the track ids of the humans interacting (relative to the GT annotation files)
+    # and b and e are the starting frame and ending frame (included) of the interaction,
+    # starting with frame 0
+    GT_file = "{}/{}.annotations".format(GT_dir, video_id)
+    interactions = []
+    with open(GT_file, "r") as f:
+        f.readline()
+        interacting = False
+        previous_frame_id = None
+
+        for line in f:
+            if line[:6] == "#frame":
+                entries = line.strip().split()
+                frame_id = int(entries[1])
+                if len(entries) > 4:
+                    id_1 = int(entries[5])
+                    id_2 = int(entries[7])
+                    if not interacting:
+                        interacting = True
+                        interactions.append([id_1, id_2, frame_id])
+                    elif [id_1, id_2] != interactions[-1][:2]:
+                        interactions[-1].append(previous_frame_id)
+                        interactions.append([id_1, id_2, frame_id])
+                else:
+                    if interacting:
+                        interacting = False
+                        interactions[-1].append(previous_frame_id)
+                previous_frame_id = frame_id
+        if interacting:
+            interacting = False
+            interactions[-1].append(previous_frame_id)
+            
+    for elt in interactions:
+        assert len(elt) == 4
+    return interactions
+
+
+def get_tracks(video_id):
+    tracks_files = sorted(glob.glob("{}/{}/*".format(tracks_dir, video_id)))
+    tracks = []
+    for tracks_file in tracks_files:
+        with open(tracks_file, "rb") as f:
+            tracks += pickle.load(f)
+    tracks = [e[0] for e in tracks]
+    return tracks
+
+
+def get_GT_to_det(video_id):
+    GT_to_det_file = "{}/{}_track_links.pkl".format(track_linking_dir, video_id)
+    with open(GT_to_det_file, "rb") as f:
+        GT_to_det = pickle.load(f)
+    return GT_to_det
+
+
+def generate_positive_pairs(video_id):
+    interactions = get_interactions(video_id)
+    tracks = get_tracks(video_id)
+    GT_to_det = get_GT_to_det(video_id)
+
+    print("Interactions:")
+    for elt in interactions: print(elt)
+    print()
+
+    pairs = []
+    for id_1, id_2, b, e in interactions:
+        if (id_1 not in GT_to_det) or (id_2 not in GT_to_det):
+            continue
+        track_1 = tracks[GT_to_det[id_1][0]]
+        track_2 = tracks[GT_to_det[id_2][0]]
+
+        inter_b = int(max(track_1[0, 0], track_2[0, 0]))
+        inter_e = int(max(track_1[-1, 0], track_2[-1, 0]))
+
+        begin = max(b - ACCEPTABLE_DELAY, inter_b)
+        end = min(e + ACCEPTABLE_DELAY, inter_e)
+
+        segment_begin_indices = list(range(begin, end - SEGMENT_LENGTH, SEGMENT_LENGTH))
+        for begin_frame in segment_begin_indices:
+            pair = []
+            pair += [video_id, GT_to_det[id_1][0], begin_frame, begin_frame + SEGMENT_LENGTH]
+            pair += [video_id, GT_to_det[id_2][0], begin_frame, begin_frame + SEGMENT_LENGTH]
+            pairs.append(pair)
+
+        print("tracks {} - {} : \t".format(id_1, id_2) + "\t".join(map(str, segment_begin_indices)))
+    
+    output_file = "{}/positive/pairs_{}.csv".format(pairs_dir, video_id)
+    Path("/".join(output_file.split("/")[:-1])).mkdir(parents=True, exist_ok=True)
+
+    with open(output_file, "w") as f:
+        for pair in pairs:
+            f.write(",".join(map(str, pair)) + "\n")
+
+
+if __name__ == "__main__":
+    # video_ids = [e.split("/")[-1] for e in sorted(glob.glob("{}/*".format(tracks_dir)))]
+    # for video_id in video_ids:
+    #     generate_positive_pairs(video_id)
+
+    video_id = sys.argv[1]
+    generate_positive_pairs(video_id)
